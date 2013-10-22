a1cabc00c148fe8a338130343315b808e97085ff
pyro2927 <joseph@pintozzi.com>
2013-10-21 21:38:45 -0500

    Working on adding and removing keys

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
 # Specify your gem's dependencies in sandman.gemspec
-gemspec
 gem 'bitbucket_rest_api', :git => "https://github.com/pyro2927/bitbucket.git"
+gemspec
--- lib/sandman.rb
+++ lib/sandman.rb
@@ -9,14 +9,28 @@ module Sandman
   def self.filter_args(args)
     Sandman.init
     if args.count > 1
-      if args[0].downcase == "add"
-        key = args[1]
-        if args.count > 2
-          name = args[2]
-        else
-          name = `hostname`.strip
-        end
+      # try to automatically detect if we're being handed a file or key string
+      key = args[1]
+      if key.start_with?("ssh") == false && File.exist?(key)
+        key = File.read(key)
+      end
+      if args.count > 2
+        name = args[2]
+      else
+        name = `hostname`.strip
+      end
+      command = args[0]
+      if command == "add"
         Sandman.add_key_to_providers(key, name)
+      elsif command.start_with?("rem") || command.start_with?("del")
+        Sandman.remove_key_from_providers(key)
+      end
+    elsif args.count == 1
+      command = args[0]
+      if command == "show"
+        Sandman.show_keys
+      else if command == "sync" || command == "merge"
+        #TODO: merge this stuff
       end
     end
   end
@@ -77,8 +91,10 @@ module Sandman
     begin
       if p.kind_of? Github::Client
         p.users.keys.create({:title => title, :key => key})
+        puts "Key added to GitHub"
       elsif p.kind_of? BitBucket::Client
         p.users.account.new_key(p.login, {:label => title, :key => key})
+        puts "Key added to BitBucket"
       end
     rescue Exception => e
       puts e.to_s
@@ -86,8 +102,34 @@ module Sandman
   end
 
   def self.add_key_to_providers(key = "", title = "")
+    puts "Adding #{key} to providers..."
     @providers.each do |p|
       Sandman.add_key_to_provider(p, key, title)
+    end
+  end
+
+  def self.remove_key_from_provider(p, key)
+    begin
+      if p.kind_of? Github::Client
+        p.users.keys.delete(key[:id])
+        puts "Key removed from GitHub"
+      elsif p.kind_of? BitBucket::Client
+        p.users.account.delete_key(p.login, key[:pk])
+        puts "Key removed from BitBucket"
+      end
+    rescue Exception => e
+      puts e.to_s
+    end
+  end
+
+  def self.remove_key_from_providers(key = "")
+    puts "Removing #{key} from providers..."
+    @providers.each do |p|
+      Sandman.keys_for_provider(p).each do |k|
+        if ( k[:key].split(" ")[0..1].join(" ").start_with?(key) || (k[:title].nil? == false && k[:title].downcase == key.downcase) || (k[:label].nil? == false && k[:label].downcase == key.downcase) )
+          Sandman.remove_key_from_provider(p, k)
+        end
+      end
     end
   end
 
@@ -95,7 +137,11 @@ module Sandman
     @providers.each do |p|
       puts p.class.name.split("::").first + ": " + p.login
       Sandman.keys_for_provider(p).each do |k|
-        puts "\t" + k[:key]
+        if p.kind_of? Github::Client
+          puts "\t#{k[:title]}: #{k[:key][0..50]}"
+        elsif p.kind_of? BitBucket::Client
+          puts "\t#{k[:label]}: #{k[:key][0..50]}"
+        end
       end
     end
   end
